978b9fff7329c0f1b92f7e24879f4c83d4cfe212
961860916@qq.com <8302812>
2019-03-02 17:52:35 +0800

    增加微信内容

---
+++ src/README.md
@@ -0,0 +1,6 @@
+# BulleyeAdminWeb
+angular-cli常用命令:
+ng generate/g module home --routing --spec=false
+ng generate/g component home/home-list --routing --spec=false
+ng generate/g service home/home --routing --spec=false
+ng new my-project --style=scss --routing -s
---
+++ src/app/modules/wechat/wx-account/wx-account.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { WxAccountRoutingModule } from './wx-account-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    WxAccountRoutingModule
+  ]
+})
+export class WxAccountModule { }
