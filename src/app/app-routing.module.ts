425afbfaa78a165c68be4107ea808a6460c7bce0
961860916@qq.com <8302812>
2019-03-03 11:22:55 +0800

    路由

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,11 @@ const routes: Routes = [
   {path: 'staff', loadChildren: './modules/system/staff/staff.module#StaffModule'},
   {path: 'menu', loadChildren: './modules/system/menu/menu.module#MenuModule'},
   {path: 'role', loadChildren: './modules/system/role/role.module#RoleModule'},
+  {path: 'account', loadChildren: './modules/system/account/account.module#AccountModule'},
+  {path: 'wxaccount', loadChildren: './modules/wechat/wx-account/wx-account.module#WxAccountModule'},
+  {path: 'wxmember', loadChildren: './modules/wechat/wx-member/wx-member.module#WxMemberModule'},
+  {path: 'wxmenu', loadChildren: './modules/wechat/wx-menu/wx-menu.module#WxMenuModule'},
+  {path: 'wxreply', loadChildren: './modules/wechat/wx-reply/wx-reply.module#WxReplyModule'},
 ];
 
 @NgModule({
