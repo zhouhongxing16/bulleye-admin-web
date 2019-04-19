439b78f5930082fa438ffa9c0da38f62d83e0f76
961860916@qq.com <8302812>
2019-04-19 16:26:30 +0800

    优化部分页面

--- src/app/modules/system/log/log-routing.module.ts
+++ src/app/modules/system/log/log-routing.module.ts
@@ -2,12 +2,9 @@
   import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LogListComponent} from './log-list/log-list.component';
-import { LogEditComponent} from './log-edit/log-edit.component';
 
 const routes: Routes = [
   {path: 'list', component: LogListComponent},
-  {path: 'edit/:id', component: LogEditComponent},
-  {path: 'add', component: LogEditComponent}
   ];
 
 @NgModule({
--- src/app/modules/system/login-record/login-record-routing.module.ts
+++ src/app/modules/system/login-record/login-record-routing.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {LoginRecordEditComponent} from './login-record-edit/login-record-edit.component';
 import {LoginRecordListComponent} from './login-record-list/login-record-list.component';
 
 const routes: Routes = [
   {path: 'list', component: LoginRecordListComponent},
-  {path: 'edit/:id', component: LoginRecordEditComponent},
-  {path: 'add', component: LoginRecordEditComponent}
 ];
 
 @NgModule({
