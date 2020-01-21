dd248fe0fec11f80dce6e102a9497bf0c5db1135
961860916@qq.com <8302812>
2020-01-21 17:46:55 +0800

    点击菜单增加loading

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -22,12 +22,14 @@ export class MenuComponent implements OnInit {
   }
 
   initAuth(obj: any) {
+    this.help.loading();
     this.help.get(`/rolemenuauth/getAuthByMenuId/` + obj.id).subscribe(msg => {
       let enc = '';
       if (msg.success && !this.help.isEmpty(msg.data)) {
         const data = JSON.stringify(msg.data);
         enc = window.btoa(data);
       }
+      this.help.stopLoad();
       this.router.navigate([obj.path], {
         queryParams: {
           code: enc
